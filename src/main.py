41ed43e8d57a920caa2695b901d81951d85cfdef
dev-capitan <dwilson@capitanappliance.com>
2024-07-30 13:13:11 -0400

    updated main to use argparse

--- src/main.py
+++ src/main.py
@@ -3,18 +3,14 @@
 # /src/ls_argv.py
 
 
-import sys
+import argparse
 from pathlib import Path
 
-if (args_count := len(sys.argv)) > 2:
-    print(f"one argument exepcted, got {args_count - 1}")
-    raise SystemExit(2)
-elif args_count < 2:
-    print("You must specify the target directory")
-    raise SystemExit(2)
+parser = argparse.ArgumentParser()
+parser.add_argument("path")
+args = parser.parse_args()
 
-
-target_dir = Path(sys.argv[1])
+target_dir = Path(args.path)
 
 if not target_dir.is_dir():
     print("The target directory doesn't exist")
